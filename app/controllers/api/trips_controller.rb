2c6ddc2661b25cecab06edd8a0c9cfb82719d6dd
Edyta Barańska <edytacr@gmail.com>
2024-06-12 18:04:18 +0200

    creating locations after saving a trip

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -7,14 +7,30 @@ module Api
 
     def create
       @trip = Trip.new(trip_params)
-      @trip.save
-      render json: @trip
+      
+      if @trip.save
+        create_start_address_location
+        create_destination_address_location
+        render json: @trip
+      else
+        render json: @trip.errors, status: :unprocessable_entity
+      end
     end
 
     private
 
     def trip_params
       params.permit(:start_address, :destination_address, :price, :date)
+    end
+
+    def create_start_address_location
+      start_location = Location.new(address: trip_params[:start_address])
+      start_location.save
+    end
+
+    def create_destination_address_location
+      destination_location = Location.new(address: trip_params[:destination_address])
+      destination_location.save
     end
   end
 end
