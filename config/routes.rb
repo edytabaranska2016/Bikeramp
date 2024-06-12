7f40e41a1c0ec69e14241cbd97ca0c1c9eb9c266
Edyta Barańska <edytacr@gmail.com>
2024-06-12 18:25:16 +0200

    api for stats is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :trips
+    resources :trips, only: [:index, :create]
+    get '/stats/weekly', to: 'stats#weekly'
+    get '/stats/monthly', to: 'stats#monthly'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
